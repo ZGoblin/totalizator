58a11e252d0a7883f5c406ccfdd99c92e85940a4
Oleh Zaiets <59374316+ZGoblin@users.noreply.github.com>
2021-03-18 16:24:40 +0200

    add di, sharedpref and leakcanary dependencies

---
+++ app/src/main/java/com/kvad/totalizator/tools/sharedPrefTools/SharedPref.kt
@@ -0,0 +1,22 @@
+package com.kvad.totalizator.tools.sharedPrefTools
+
+import android.content.Context
+import android.content.SharedPreferences
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class SharedPref @Inject constructor(context: Context) {
+
+    private companion object {
+        const val PRIVATE_SHARED_PREF = "PRIVATE_SHARED_PREF"
+        const val KEY_TOKEN = "KEY_TOKEN"
+        const val DEFAULT_VALUE = ""
+    }
+
+    private val sharedPreferences: SharedPreferences by lazy {
+        context.getSharedPreferences(PRIVATE_SHARED_PREF, Context.MODE_PRIVATE)
+    }
+
+    var token: String by SharedPrefDelegate(sharedPreferences, KEY_TOKEN, DEFAULT_VALUE)
+}
