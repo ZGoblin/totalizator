70fa2a658b5036704f577950eecd206ff195d098
Oleh Zaiets <59374316+ZGoblin@users.noreply.github.com>
2021-03-22 17:40:28 +0200

    add flow

--- app/src/main/java/com/kvad/totalizator/data/UserRepository.kt
+++ app/src/main/java/com/kvad/totalizator/data/UserRepository.kt
@@ -7,30 +7,33 @@ import com.kvad.totalizator.data.models.RegisterRequest
 import com.kvad.totalizator.data.models.Token
 import com.kvad.totalizator.data.models.Wallet
 import com.kvad.totalizator.shared.ResultWrapper
+import com.kvad.totalizator.tools.REQUEST_DELAY
 import com.kvad.totalizator.tools.safeApiCall
 import com.kvad.totalizator.tools.sharedPrefTools.SharedPref
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class UserRepository @Inject constructor(
     private val userService: UserService,
     private val sharedPref: SharedPref
 ) {
-    suspend fun login(loginRequest: LoginRequest) : ResultWrapper<Token> {
+    suspend fun login(loginRequest: LoginRequest): ResultWrapper<Token> {
         return safeApiCall {
             userService.login(loginRequest)
         }
     }
 
-    suspend fun register(registerRequest: RegisterRequest) : ResultWrapper<Token> {
+    suspend fun register(registerRequest: RegisterRequest): ResultWrapper<Token> {
         return safeApiCall {
             userService.register(registerRequest)
         }
     }
 
-    suspend fun wallet() : ResultWrapper<Wallet> {
-        return safeApiCall {
-            Log.d("safeApiCall", sharedPref.token)
-            userService.wallet()
+    suspend fun wallet(): Flow<ResultWrapper<Wallet>> = flow {
+        while (true) {
+            emit(safeApiCall(userService::wallet))
+            kotlinx.coroutines.delay(REQUEST_DELAY)
         }
     }
 
