2e532ff267e9d47712e31beb7b965876fd5aaac2
Rodion <rodion.gramushniak@gmail.com>
2021-03-22 17:50:17 +0200

    add flow in repo

--- app/src/main/java/com/kvad/totalizator/data/EventRepository.kt
+++ app/src/main/java/com/kvad/totalizator/data/EventRepository.kt
@@ -7,8 +7,14 @@ import com.kvad.totalizator.data.models.Event
 import com.kvad.totalizator.data.models.ParticipantDTO
 import com.kvad.totalizator.shared.ResultWrapper
 import com.kvad.totalizator.tools.safeApiCall
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
+import javax.inject.Singleton
+import kotlin.random.Random
 
+@Singleton
 class EventRepository @Inject constructor(
     private val eventService: EventService
 ) {
@@ -19,27 +25,54 @@ class EventRepository @Inject constructor(
         }
     }
 
+
+    fun setId(id: String): Flow<ResultWrapper<Event>> {
+        latestNews = flow {
+            while (true) {
+                val latestNews = getEventById(id = id)
+                emit(latestNews)
+                delay(3000)
+            }
+        }
+
+        return latestNews
+    }
+
+    var latestNews: Flow<ResultWrapper<Event>> = flow {}
+        private set
+
     @Suppress("UnusedPrivateMember")
     suspend fun getEventById(id: String): ResultWrapper<Event> {
+
+        val random1 = Random(300).nextFloat()
+        val random2 = Random(300).nextFloat()
+        val random3 = Random(300).nextFloat()
+
         return safeApiCall {
-            return@safeApiCall Event(
-                "id",
+            Event(
+                id,
                 "sport",
                 ParticipantDTO(
-                    1, "Olexiy", "https://upload.wikimedia.org/wikipedia/commons/a/a9/Olexiy_yurin.jpg", setOf(
+                    1,
+                    "Olexiy",
+                    "https://upload.wikimedia.org/wikipedia/commons/a/a9/Olexiy_yurin.jpg",
+                    setOf(
                         Characteristic("weight", "55"),
                         Characteristic("height", "55"),
                         Characteristic("age", "55")
                     )
                 ),
                 ParticipantDTO(
-                    1, "Rodion", "https://stuki-druki.com/biofoto/Rodion-Tolokonnikov-Ryadovie.jpg", setOf(
+                    1,
+                    "Rodion",
+                    "https://stuki-druki.com/biofoto/Rodion-Tolokonnikov-Ryadovie.jpg",
+                    setOf(
                         Characteristic("weight", "55"),
                         Characteristic("height", "55"),
                         Characteristic("age", "55")
                     )
                 ),
-                BetPool(1F, 1F, 1F)
+                BetPool(random1, random2, random3)
             )
         }
     }
