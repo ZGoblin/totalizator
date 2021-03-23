1e9f63c6a0f67bce38b908ef364517d98bc32014
Oleh Zaiets <59374316+ZGoblin@users.noreply.github.com>
2021-03-23 14:03:28 +0200

    fix leak on login and registration

--- app/src/main/java/com/kvad/totalizator/login/LoginFragment.kt
+++ app/src/main/java/com/kvad/totalizator/login/LoginFragment.kt
@@ -10,6 +10,7 @@ import com.afollestad.materialdialogs.MaterialDialog
 import com.kvad.totalizator.App
 import com.kvad.totalizator.R
 import com.kvad.totalizator.data.models.LoginRequest
+import com.kvad.totalizator.databinding.EventsFragmentBinding
 import com.kvad.totalizator.databinding.LoginFragmentBinding
 import javax.inject.Inject
 
@@ -17,14 +18,15 @@ class LoginFragment : Fragment() {
 
     @Inject
     lateinit var viewModel: LoginViewModel
-    private lateinit var binding: LoginFragmentBinding
+    private var _binding: LoginFragmentBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = LoginFragmentBinding.inflate(inflater, container, false)
+        _binding = LoginFragmentBinding.inflate(inflater, container, false)
         return binding.root
     }
 
@@ -99,5 +101,10 @@ class LoginFragment : Fragment() {
     private fun setupDi() {
         val app = requireActivity().application as App
         app.getComponent().inject(this)
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
     }
 }
