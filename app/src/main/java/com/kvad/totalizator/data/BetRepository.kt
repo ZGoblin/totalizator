06ebb276e459ac367656b54c49b00a8efebad3b5
OlexiyTr <58363925+OlexiyTr@users.noreply.github.com>
2021-03-23 22:08:29 +0200

    Add imports

--- app/src/main/java/com/kvad/totalizator/data/BetRepository.kt
+++ app/src/main/java/com/kvad/totalizator/data/BetRepository.kt
@@ -3,7 +3,10 @@ package com.kvad.totalizator.data
 import com.kvad.totalizator.tools.safeapicall.ApiResultWrapper
 import com.kvad.totalizator.betfeature.BetRequest
 import com.kvad.totalizator.data.api.UserService
+import com.kvad.totalizator.data.requestmodels.Wallet
 import com.kvad.totalizator.tools.safeapicall.safeApiCall
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.single
 import javax.inject.Inject
 
 class BetRepository @Inject constructor(
