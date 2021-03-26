1c31191e39e23b3cf205737085144f0ab777d9d4
Oleh Zaiets <59374316+ZGoblin@users.noreply.github.com>
2021-03-26 12:31:01 +0200

    Update MapEventToDetailUiModel.kt

--- app/src/main/java/com/kvad/totalizator/detail/MapEventToDetailUiModel.kt
+++ app/src/main/java/com/kvad/totalizator/detail/MapEventToDetailUiModel.kt
@@ -1,6 +1,7 @@
 package com.kvad.totalizator.detail
 
 import com.kvad.totalizator.data.model.Event
+import com.kvad.totalizator.data.requestmodels.Characteristic
 import com.kvad.totalizator.detail.model.EventDetail
 import java.time.ZonedDateTime
 import javax.inject.Inject
@@ -31,16 +32,47 @@ class MapEventToDetailUiModel @Inject constructor() {
             )
         }
 
-        event.firstParticipant.characteristics.forEach { firstPlayerCharacteristic ->
-            event.secondParticipant.characteristics.forEach { secondPlayerCharacteristic ->
-                if (firstPlayerCharacteristic.type == secondPlayerCharacteristic.type){
-                    resultList.add(EventDetail.CharacteristicUiModel(
-                        firstPlayerCharacteristic.type,
-                        firstPlayerCharacteristic.value,
-                        secondPlayerCharacteristic.value
-                    ))
-                }
+        val second = event.secondParticipant.characteristics
+        val first = event.firstParticipant.characteristics
+//        val second = setOf(Characteristic("height", "180"), Characteristic("weight", "65"))
+//        val first = setOf(Characteristic("height", "165"), Characteristic("weight", "80"), Characteristic("Age", "65"))
+
+        val parameters = mutableListOf<EventDetail.CharacteristicUiModel>()
+        first.forEach { characteristic ->
+            parameters.add(
+                EventDetail.CharacteristicUiModel(
+                    characteristicName = characteristic.type,
+                    firstPlayerValue = characteristic.value,
+                    secondPlayerValue = ""
+                )
+            )
+        }
+
+        second.forEach { playerCharacteristic ->
+            parameters.find { characteristic ->
+                characteristic.characteristicName == playerCharacteristic.type
+            }?.let {
+                parameters.add(
+                    EventDetail.CharacteristicUiModel(
+                        characteristicName = playerCharacteristic.type,
+                        firstPlayerValue = it.firstPlayerValue,
+                        secondPlayerValue = playerCharacteristic.value
+                    )
+                )
+                parameters.remove(it)
+                return@forEach
             }
+            parameters.add(
+                EventDetail.CharacteristicUiModel(
+                    characteristicName = playerCharacteristic.type,
+                    firstPlayerValue = "",
+                    secondPlayerValue = playerCharacteristic.value
+                )
+            )
+        }
+
+        parameters.forEach {
+            resultList.add(it)
         }
 
         return resultList
