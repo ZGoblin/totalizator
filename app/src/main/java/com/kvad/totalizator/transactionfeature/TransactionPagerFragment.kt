13fa94adf9140a92d4b167384d41eda06c9ad652
OlexiyTr <58363925+OlexiyTr@users.noreply.github.com>
2021-03-27 19:56:17 +0200

    add some strings to res

--- app/src/main/java/com/kvad/totalizator/transactionfeature/TransactionPagerFragment.kt
+++ app/src/main/java/com/kvad/totalizator/transactionfeature/TransactionPagerFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.google.android.material.tabs.TabLayoutMediator
 import com.kvad.totalizator.databinding.TransactionViewPagerFragmentBinding
 import com.kvad.totalizator.transactionfeature.adapter.TransactionPagerAdapter
